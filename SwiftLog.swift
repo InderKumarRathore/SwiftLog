47ecb5429a9833887b1e78ecb1f646340975f51b
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2015-10-07 17:07:52 +0530

    Adding SwiftLog.swift file

---
+++ SwiftLog.swift
@@ -0,0 +1,22 @@
+//
+//  Log.swift
+//  Swift2
+//
+//  Created by Inder Kumar Rathore on 07/10/15.
+//  Copyright © 2015 Inder Kumar Rathore. All rights reserved.
+//
+// Have an issue? Please visit : https://github.com/InderKumarRathore/SwiftLog
+// and file a bug
+
+import Foundation
+
+/// Logs the message to the console with extra information
+///
+/// To make it work you must set the "DEBUG" symbol, set it in the "Swift Compiler - Custom Flags" section, "Other Swift Flags" line.
+/// You add the DEBUG symbol with the -D DEBUG entry.
+func log(message: String, functionName: String = __FUNCTION__, fileName: String = __FILE__, lineNumber: Int = __LINE__) {
+  #if DEBUG
+    let className = (fileName as NSString).lastPathComponent
+    print("<\(className)> \(functionName) [#\(lineNumber)]| \(message)")
+  #endif
+}
